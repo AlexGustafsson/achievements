0a66526560e3941d2a76dcc05cf7f9ca5105ece3
Alex Gustafsson <algc16@student.bth.se>
2020-02-08 18:44:40 +0100

    Implement you see my point achievement

--- src/hooks/comment.js
+++ src/hooks/comment.js
@@ -1,3 +1,22 @@
-module.exports = [
+const utils = require('../utils');
+const {parseUser} = utils.bodyParser;
+const {userHasAchievement} = utils.state;
 
+/**
+* Check for completion for the 'You see my point?' achievement.
+* @param state {Object} - The state object to use.
+* @param body {Object} - The parsed JSON body received from GitLab.
+*/
+async function checkYouSeeMyPoint(state, body) {
+  const user = parseUser(body);
+  if (userHasAchievement(state, user, 'You see my point?'))
+    return;
+
+  // Unlock the achievement if a user has commented on a merge request
+  if (body['merge_request'])
+    return {user, achievement: 'You see my point?'};
+}
+
+module.exports = [
+  checkYouSeeMyPoint
 ];
