f3daeb26742f1694de43f626a73caa23f7658654
Alex Gustafsson <algc16@student.bth.se>
2020-03-25 15:07:09 +0100

    Add reviewer ranks

--- src/hooks/comment.js
+++ src/hooks/comment.js
@@ -17,6 +17,52 @@ async function checkYouSeeMyPoint(userStore, body) {
     return [{user, achievement: 'You see my point?'}];
 }
 
+/**
+* Check for completion for the '<Rank> Reviewer' achievements.
+* @param userStore {Object} - The user store object to use.
+* @param body {Object} - The parsed JSON body received from GitLab.
+* @returns {Array} - An array of unlocked achievements.
+*/
+async function checkReviewerRank(userStore, body) {
+  const user = parseUser(body);
+  if (await userStore.userHasAchievement(user, 'Grandmaster Reviewer'))
+    return [];
+
+  // Skip comments not made on merge requests
+  if (!body['merge_request'])
+    return [];
+
+  const commentAuthorId = body['object_attributes']['author_id'];
+  const mergeRequestAuthorId = body['merge_request']['author_id'];
+
+  // Skip comments on the author's own merge request
+  if (commentAuthorId === mergeRequestAuthorId)
+    return [];
+
+  const metadata = await userStore.getMetadata(user, 'Reviewer Rank');
+  metadata['comments'] = metadata['comments'] || 0;
+  metadata['comments'] += 1;
+  await userStore.setMetadata(user, 'Reviewer Rank', metadata);
+
+  console.log(user.username, metadata);
+
+  if (metadata['comments'] === 200) {
+    // Remove metadata
+    await userStore.clearMetadata(user, 'Reviewer Rank');
+    return [{user, achievement: 'Grandmaster Reviewer'}];
+  }
+
+  if (metadata['comments'] === 150)
+    return [{user, achievement: 'Master Reviewer'}];
+
+  if (metadata['comments'] === 75)
+    return [{user, achievement: 'Adept Reviewer'}];
+
+  if (metadata['comments'] === 25)
+    return [{user, achievement: 'Apprentice Reviewer'}];
+}
+
 module.exports = [
-  checkYouSeeMyPoint
+  checkYouSeeMyPoint,
+  checkReviewerRank
 ];
